52d601ed473204cf7fc6c719dd157d21030d065d
kaito8958 <shimo.oka8958@gmail.com>
2025-10-22 00:38:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                                 message: 'は半角英数字をそれぞれ含めて設定してください' }
+
+  validates :last_name, :first_name, presence: true,
+                                     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                                               message: 'は全角文字で入力してください' }
+
+  validates :last_name_kana, :first_name_kana, presence: true,
+                                               format: { with: /\A[ァ-ヶー－]+\z/,
+                                                         message: 'は全角カタカナで入力してください' }
+
+  validates :birthday, presence: true
 end
