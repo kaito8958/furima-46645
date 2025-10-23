854e5a64799994ff42122e2c5e0c144043cb0f8c
kaito8958 <shimo.oka8958@gmail.com>
2025-10-23 14:09:04 +0900

    販売手数料と販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+document.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  if (!priceInput) return; 
+
+  priceInput.addEventListener("input", () => {
+    const price = priceInput.value;
+    const tax = Math.floor(price * 0.1);
+    const profit = price - tax;
+
+    document.getElementById("add-tax-price").textContent = tax;
+    document.getElementById("profit").textContent = profit;
+  });
+});
