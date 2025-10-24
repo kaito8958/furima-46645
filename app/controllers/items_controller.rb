a51a6bf885ad1bc88e7e9464b99dde35a3b96a39
kaito8958 <shimo.oka8958@gmail.com>
2025-10-24 18:33:25 +0900

    Revert "商品情報編集機能の実装"
    This reverts commit 51f3024c58f8e387a6f0125cda67d8df17458fd5.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -21,17 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    @item = Item.find(params[:id])
   end
 
   private
@@ -41,13 +30,5 @@ class ItemsController < ApplicationController
       :name, :description, :category_id, :condition_id, :shipping_fee_id,
       :prefecture_id, :delivery_day_id, :price, :image
     )
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to root_path unless current_user == @item.user
   end
 end
