08443d7797ef3b2c60e06cedc0f837817d107073
kaito8958 <shimo.oka8958@gmail.com>
2025-10-28 15:20:46 +0900

    order,addressのモデルとテーブル作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,31 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name,
+                :phone_number, :user_id, :item_id, :token
+
+  # ↓ バリデーション
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  # ↓ 保存処理（OrderとAddressを同時に保存）
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      house_number: house_number,
+      building_name: building_name,
+      phone_number: phone_number,
+      order_id: order.id
+    )
+  end
+end
