3fae1fdae84635a318119949cc232ee767b99b05
kaito8958 <shimo.oka8958@gmail.com>
2025-10-29 11:18:54 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,14 +5,17 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +26,7 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :house_number,
       :building_name, :phone_number
-    ).merge(user_id: current_user.id, item_id: @item.id)
+    ).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
   end
 
   def set_item
@@ -34,5 +37,14 @@ class OrdersController < ApplicationController
     return unless @item.user_id == current_user.id || @item.order.present?
 
     redirect_to root_path
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
