4fcb9876061da8385d8a55facd2d5ef939a24310
kaito8958 <shimo.oka8958@gmail.com>
2025-10-31 17:07:55 +0900

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,11 @@
+require 'cgi'
+begin
+  CGI.accept_charset = 'UTF-8'
+rescue NameError
+  # Ruby 3.2 の未定義クラス変数ケースに備え、強制初期化
+  CGI.send(:class_variable_set, :@@accept_charset, 'UTF-8')
+end
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
