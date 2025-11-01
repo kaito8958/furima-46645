abac16d9dad9d564f0d494d855657533e0b03637
kaito8958 <shimo.oka8958@gmail.com>
2025-11-01 18:38:44 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.95.75.206 ', user: 'ec2-user', roles: %w{app db web}
+server '54.95.75.206', user: 'ec2-user', roles: %w{app db web}
